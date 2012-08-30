21983b97ff48fe2a706c1b468d1b165d3d9e0df6
Daniel Rashid <drashid@squarespace.com>
2012-08-29 21:57:15 -0400

    Huge dense arrays work without holding a bunch in memory and doing O(n) lookups using nth.  Test added to confirm which drops the first million and takes 1.

--- test/machina/data_test.clj
+++ test/machina/data_test.clj
@@ -3,7 +3,7 @@
         machina.data))
 
 (deftest one-dense-test
-  (testing "Two dense vectors combined"
+  (testing "Two dense fragments combined together"
     (is (=
          (combined-seq [(dense-fragment (take 2 (range)))
                         (dense-fragment (take 3 (range)) 3)])
@@ -11,8 +11,14 @@
 
 
 (deftest mix-test
-  (testing "One dense vector and one sparse vector combined"
+  (testing "One dense fragment and one sparse fragment combined"
     (is (=
          (combined-seq [(dense-fragment (take 2 (range)))
                         (sparse-fragment {1 "a"} 2)])
          [[0 0] [1 1] [2 0] [3 "a"]]))))
+
+(deftest huge-dense
+  (testing "Huge dense fragment!  Testing that we're not doing any O(n) operations or holding a ton in memory"
+    (is (=
+         (take 1 (drop 1000000 (combined-seq [(dense-fragment (range) 100000000)])))
+         [[1000000 1000000]]))))
