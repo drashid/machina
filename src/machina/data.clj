c5e55891421ccc094d903f4d8d541532f7313f2f
Daniel Rashid <drashid@squarespace.com>
2012-08-29 22:07:33 -0400

    Docs/Comments

--- src/machina/data.clj
+++ src/machina/data.clj
@@ -3,12 +3,12 @@
 (defrecord ArrayFragment [data length type])
 
 (defn- dense?
-  "A dense vector is a sequential collection of items"
+  "A dense fragment is a sequential collection of items"
   [frag]
   (= :dense (:type frag)))
 
 (defn- sparse?
-  "A sparse vector is a map from {int->item}"
+  "A sparse fragment is a map from {int->item}"
   [frag]
   (= :sparse (:type frag)))
 
@@ -32,12 +32,18 @@
   (let [current (first arr-frag-seq)]
     (if (and (not (empty? arr-frag-seq)) (not (nil? current)))
       (cond
+       ; If we've reached the end of the current fragment, move on to the next one,
+       ; incrementing the global index and reverting the local index to 0
        (<= (:length current) lidx)
        (lazy-seq (combined-seq-helper (rest arr-frag-seq) (+ gidx (:length current)) 0))
+       ; If we're sparse, pull out our data value from the map or default to 0
        (sparse? current)
        (let [data-val (get (:data current) lidx 0)
              output-pair [(+ lidx gidx) data-val]]
          (lazy-seq (cons output-pair (combined-seq-helper arr-frag-seq gidx (inc lidx)))))
+       ; If we're dense, to handle very long lists without an O(n) lookup, we're actually going to
+       ; use the current value, then discard it and reposition the head of our :data to the next element
+       ; the local index will then serve to compute the global output index, but not be used for lookup
        (dense? current)
        (let [data-val (first (:data current))
              next-seq (cons (assoc current :data (rest (:data current))) (rest arr-frag-seq))
