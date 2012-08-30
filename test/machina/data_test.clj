bbd8b1fe4ba0104ba443b63b90b1898905ffd5d7
Daniel Rashid <drashid@squarespace.com>
2012-08-29 21:52:56 -0400

    test with mixing dense and sparse

--- test/machina/data_test.clj
+++ test/machina/data_test.clj
@@ -3,8 +3,16 @@
         machina.data))
 
 (deftest one-dense-test
-  (testing "Something!"
+  (testing "Two dense vectors combined"
     (is (=
          (combined-seq [(dense-fragment (take 2 (range)))
-                        (dense-fragment (take 1 (range)))])
-         [[0 0] [1 1] [2 0]]))))
+                        (dense-fragment (take 3 (range)) 3)])
+         [[0 0] [1 1] [2 0] [3 1] [4 2]]))))
+
+
+(deftest mix-test
+  (testing "One dense vector and one sparse vector combined"
+    (is (=
+         (combined-seq [(dense-fragment (take 2 (range)))
+                        (sparse-fragment {1 "a"} 2)])
+         [[0 0] [1 1] [2 0] [3 "a"]]))))
