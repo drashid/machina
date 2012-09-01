3643dab7024bcab032e91199b4b4fcaf375b9775
Daniel Rashid <drashid@squarespace.com>
2012-08-31 23:39:00 -0400

    Attribute test to double check set contains check

--- test/machina/attribute_test.clj
+++ test/machina/attribute_test.clj
@@ -8,10 +8,10 @@
     (is (not (attr/validate (attr/numeric "numeric") "not a number")))
     ))
 
-
 (deftest validation-set
   (testing "Testing validations for set attribute"
     (is (attr/validate (attr/nominal "set" ["A" "B"]) "A"))
+    (is (not (attr/validate (attr/nominal "set" ["A" "B"]) "C")))
     (is (not (attr/validate (attr/nominal "set" ["A" "B"]) 1)))
     ))
 
