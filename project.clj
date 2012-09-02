87cc4a2d5afa9a11b34f7c592ee8d8c3ec1aa768
Daniel Rashid <drashid@squarespace.com>
2012-09-02 00:10:33 -0400

    lein jvm opts

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clojure-csv "2.0.0-alpha2"]])
+                 [clojure-csv "2.0.0-alpha2"]]
+  :jvm-opts ["-Xmx1g" "-server"])
